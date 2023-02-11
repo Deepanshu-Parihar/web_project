e45a8a2abbe60883a95ebc6df59c73d0107eeff2
Deepanshu Parihar <parihardeepanshu710@gmail.com>
2023-02-12 02:35:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function cookmaggie(maggie,cup,pan){
+//     console.log("Your maggie will be ready in "
+//     +maggie*2+" minutes"
+//     +" and ingredients are "
+//     + maggie + cup +" cups of water"
+//     + "and" +pan +" pan");
+// }
+
+// cookmaggie(1,2,1)
+
